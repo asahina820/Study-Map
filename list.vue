08d7bff130b1a38adc17c2dc3adfc07e72c44e38
hinamei <hina.gntm@gmail.com>
2022-04-23 00:48:22 +0900

    地図画面と一覧画面をvueにリプレース

---
+++ list.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="ui four cards" style="width:80%; margin-top:50px; margin-left: auto; margin-right: auto;">
+    <div class="card">
+      <div class="image">
+        <img src="https://www2.wagmap.jp/sendaicity-html/pict/ext_shimin.jpg">
+      </div>
+      <div class="content">
+        <p class="header">{{ title }}</p>
+      </div>
+      <div class="extra">
+        Rating:
+        <div class="ui star rating" data-rating="4"></div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image">
+        <img src="https://www2.wagmap.jp/sendaicity-html/pict/ext_shimin.jpg">
+      </div>
+      <div class="content">
+        <p class="header">{{ title }}</p>
+      </div>
+      <div class="extra">
+        Rating:
+        <div class="ui star rating" data-rating="4"></div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image">
+        <img src="https://www2.wagmap.jp/sendaicity-html/pict/ext_shimin.jpg">
+      </div>
+      <div class="content">
+        <p class="header">{{ title }}</p>
+      </div>
+      <div class="extra">
+        Rating:
+        <div class="ui star rating" data-rating="4"></div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image">
+        <img src="https://www2.wagmap.jp/sendaicity-html/pict/ext_shimin.jpg">
+      </div>
+      <div class="content">
+        <p class="header">{{ title }}</p>
+      </div>
+      <div class="extra">
+        Rating:
+        <div class="ui star rating" data-rating="4"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      title: '宮城県図書館'
+    }
+  }
+}
+</script>
--- main.js
+++ main.js
@@ -1,38 +1,10 @@
-// 背景地図表示
-let lat = 38.26889; // 緯度
-let lon = 140.87194; // 経度
-let zoom = 16; // ズームレベル
-
-let map = L.map("map");
-map.setView([lat, lon], zoom);
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  {
-    attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }
-).addTo(map);
-
-// 地物表示
-var libraryIcon = L.icon({
-  iconUrl: './img/book.png',
-  iconSize: [40, 40],
-  iconAnchor: [25, 50],
-  popupAnchor: [0, -50],
-});
-
-let point;
-$.getJSON("geojson/sendai_library.geojson",function(data) {
-  point = L.geoJson(data,
-    {
-      pointToLayer: function (feature, latlng) {
-        return L.marker(latlng, {icon: libraryIcon})
-      },
-      onEachFeature: function(feature,layer){
-        let name = feature.properties.名称;
-        // TODO: 各施設の詳細ページに飛ばす。今は仮のページになっている
-        let link = './detail.html'
-        layer.bindPopup(name + '<br>' + '<a href = ' + link + '>詳細</a>');
-      }
-    }
-  ).addTo(map);
+const router = new VueRouter({
+  routes: [
+      { path: '/list', component: httpVueLoader('./list.vue') },
+      { path: '/', component: httpVueLoader('./map.vue') },
+  ]
+})
+const app = new Vue({
+  el: "#app",
+  router,
 });
