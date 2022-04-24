e76730688cd22c0304483d2022f1a5acc11da54c
Akihiro MATOBA <matobaa+github@gmail.com>
2022-04-24 19:29:56 +0900

    Firebaseへ接続する (#3)
    * WIP
    
    * use ver.8 (namespace) instead of ver.9 (module)

--- detail.vue
+++ detail.vue
@@ -20,6 +20,20 @@
                 <a class="ui label">人気</a>
             </div>
         </div>
+
+        <div>
+            <span v-for="comment in comments" :key="comment">{{comment}}<br/></span>
+        </div>
+        
+        <div class="extra content">
+            <p>口コミ</p>
+            <form class="ui form">
+                <div class="field">
+                    <input type="text" required>
+                </div>
+            </form>
+                <button class="ui button" type="submit"  @click="aaa">Submit</button>
+        </div>
     </div>
 </template>
 
@@ -28,7 +42,27 @@ module.exports = {
   data: function() {
     return {
       name: '宮城県図書館',
-      description: ' 伊達家ゆかりの資料である伊達文庫などの古書を含む、約115万点の資料を収蔵している。'
+      description: ' 伊達家ゆかりの資料である伊達文庫などの古書を含む、約115万点の資料を収蔵している。',
+      comments: [
+          '静かです',
+          '子どもが走り回っていました',
+          'リファレンスが優しかった'
+      ],
+    }
+  },
+  mounted: function() {
+  },
+  methods: {
+      aaa: async function() {
+        const db = this.$parent.$options.db;
+        const comment = document.querySelector('form.ui input').value;
+        const documentId = 'gHWHGxflto2fijt4Ftgs';  // FIXME: いま開いている地物のdocumentIDをもらってくること
+        const reviews = db.collection('feature').doc(documentId).collection('reviews');
+        const docRef = await reviews.add({
+            comment: comment,
+            user_id: 'TBD'
+        });
+        return false;  // TODO: リダイレクトさせる
     }
   }
 }
