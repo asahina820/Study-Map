cf080741284de918537e4a9acca9b9115fe60be3
Akihiro MATOBA <matobaa+github@gmail.com>
2022-05-01 00:24:15 +0900

    add signout

--- login.vue
+++ login.vue
@@ -1,30 +1,54 @@
 <template>
     <div id="login">
         <div id="firebaseui-auth-container"></div>
+        <div id="profile" v-if="!this.$parent?.user?.isCentinel">
+            <div class="ui list">
+                <div class="item">
+                    <img class="content" :src="this.$parent?.user?.photoURL" style="width:200px" />
+                </div>
+                <div class="item">
+                    <div class="content">{{ this.$parent?.user?.displayName || "void" }}
+                    </div>
+                </div>
+            </div>
+            <button @click="signout">sign out</button>
+        </div>
     </div>
 </template>
 
 <script>
 module.exports = {
+    data: function () {
+        return {
+            user: this.$parent?.user
+        }
+    },
     async mounted() {
-        // https://firebase.google.com/docs/auth/web/firebaseui?hl=ja#oauth_providers_google_facebook_twitter_and_github
-        var ui = new firebaseui.auth.AuthUI(firebase.auth());
-        ui.start('#firebaseui-auth-container', {
-            signInOptions: [
-                // List of OAuth providers supported.
-                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-                // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-                firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-                firebase.auth.GithubAuthProvider.PROVIDER_ID
-            ],
-            signInSuccessUrl: window.location.origin + window.location.pathname,
-            // Other config options...
-        });
-        // // https://firebase.google.com/docs/auth/web/google-signin?hl=ja
-        // // enables google provider via https://console.firebase.google.com/u/3/project/slog-map/authentication/providers?hl=ja
-        // var provider = new firebase.auth.GoogleAuthProvider();
-        // // We choose signInWithRedirect instead of signInWithPopup because popup permission needed on each users web browser
-        // firebase.auth().signInWithRedirect(provider);
+        if (this.$parent?.user?.isCentinel) {
+            this.signin();
+        }
+    },
+    methods: {
+        signin() {
+            // sign in
+            // https://firebase.google.com/docs/auth/web/firebaseui?hl=ja#oauth_providers_google_facebook_twitter_and_github
+            var ui = new firebaseui.auth.AuthUI(firebase.auth());
+            ui.start('#firebaseui-auth-container', {
+                signInOptions: [
+                    // List of OAuth providers supported.
+                    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                    // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+                    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+                    firebase.auth.GithubAuthProvider.PROVIDER_ID
+                ],
+                signInSuccessUrl: window.location.origin + window.location.pathname,
+                // Other config options...
+            });
+        },
+        signout() {
+            firebase.auth().signOut().then(this.signin);
+        }
     }
 }
 </script>
