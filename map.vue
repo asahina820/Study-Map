0aad7206a011bbeec21c1ce9373dedc7a05bb9f4
Akihiro MATOBA <matobaa+github@gmail.com>
2022-05-05 22:16:42 +0900

    Query by range with geohash (#32)
    * add geojson file parser to crud page
    
    * WIP: use geohash
    
    * debounce on move
    
    * query filter by geohash

--- map.vue
+++ map.vue
@@ -5,129 +5,163 @@
 
 <script>
 module.exports = {
-  async mounted() {
-    // 背景地図表示
-    let map = L.map("map");
+    async mounted() {
+        // 背景地図表示
+        let map = L.map("map");
 
-    map.setView([
+        map.setView([
             Number(sessionStorage.currentLat ||  38.26889),  // 緯度
             Number(sessionStorage.currentLng || 140.87194), // 経度
         ],
-        Number(sessionStorage.currentZoom || 16),  // ズームレベル
-    );
+            Number(sessionStorage.currentZoom || 16),  // ズームレベル
+        );
 
-    map.on('move', function(e){
-        currentPosi = map.getCenter();
-        currentZoom = map.getZoom();
-        sessionStorage.setItem('currentLat',currentPosi.lat);
-        sessionStorage.setItem('currentLng',currentPosi.lng);
-        sessionStorage.setItem('currentZoom',currentZoom);
-    });
-
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-    {
-        attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }
-    ).addTo(map);
-    // アイコン設定
-    var libraryIcon = L.icon({
-        iconUrl: './img/book.png',
-        iconSize: [40, 40],
-        iconAnchor: [25, 50],
-        popupAnchor: [0, -50],
-    });
-    var cafeIcon = L.icon({
-        iconUrl: './img/cafe.png',
-        iconSize: [40, 40],
-        iconAnchor: [25, 50],
-        popupAnchor: [0, -50],
-    });
-    const geojson = {
-        "type": "FeatureCollection",
-        "name": "sendai_library",
-        "features": [],
-    }
-    // retrieve features from firestore
-    const db = firebase.firestore();
-    const features = await db.collection('feature').get();
-    geojson.features.push(...features.docs.map(doc => {
-        const data = doc.data();
-        return data.geometry
-        ? {  // from geopoint
-            id: doc.id,
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    data.geometry.longitude,
-                    data.geometry.latitude,
-                ],
-            },
-            "properties": {
-                name: data.name,
-                imgSrc: data.imgSrc,
-                type: data.type,
-            },
-        }
-        : Object.assign({id: doc.id}, JSON.parse(data.geojson || "{}"));  // from geojson
-    }));
-
-    // retrieve features from spreadsheet
-    if (false) try {
-        await new Promise(resolve => setTimeout(resolve, 500));
-        while(!gapi.client.sheets) {
-            console.debug("waiting for that gapi.client.sheets api be available...")
-            await new Promise(resolve => setTimeout(resolve, 50));
-        }
-
-        const response = await gapi.client.sheets.spreadsheets.values.get({
-            spreadsheetId: '1JWUkyuRUprYrODsa1Fr70VOIxleLNy5g3Xs-g7NymmI',
-            range: 'amenity:library'
-        });
-        var features_json = response.result?.values?.slice(2)?.map(feature => {
-            const [id, lat, lng, type, name, description, imgSrc] = feature;
-            return {
-                type: "Feature",
-                id,
-                properties: { "名称": name, type, name, description, imgSrc },
-                geometry: {
-                    type: "Point", 
-                    coordinates: [ lat, lng ]
+        map.on('move', _.debounce(async (e) => {
+            currentPosi = map.getCenter();
+            currentZoom = map.getZoom();
+            sessionStorage.setItem('currentLat',currentPosi.lat);
+            sessionStorage.setItem('currentLng',currentPosi.lng);
+            sessionStorage.setItem('currentZoom',currentZoom);
+            // retrieve features for new boundaries
+            if (false) {
+                // データ量が増えてきたら有効化する
+                geoJsonLayer?.remove();
+                if (currentZoom > 8) {
+                    geoJsonLayer = await this.getFeaturesLayer();
+                    geoJsonLayer.addTo(map);
                 }
-            };
-        }) || [];
-        geojson.features.push(...features_json);
-    } catch (error) {
-        // do nothing...
-    }
-
-    L.geoJson(geojson,
-        {
-            pointToLayer: function (feature, latlng) {
-                // typeが図書館の場合は図書館のアイコンを表示する
-                if(feature.properties.type == 'library'){
-                    return L.marker(latlng, {icon: libraryIcon})
-                }
-                // 図書館以外の場合はカフェのアイコンを表示する
-                // TODO: 公民館が増えた場合は判定を追加する
-                else{
-                    return L.marker(latlng, {icon: cafeIcon})
-                }
-            },
-            onEachFeature: function(feature,layer){
-                // Leafletのpopupからrouter-linkを表示することができない
-                // そのため、Vueオブジェクトを作ってpopupに渡す
-                let name = feature.properties.name;
-                let PopupCont = Vue.extend({
-                    router,
-                    template: `<router-link to="/detail/${feature.id}" title="詳細">${name}</router-link>`
-                })
-                let popup = new PopupCont()
-                layer.bindPopup(popup.$mount().$el)
             }
-        }
-    ).addTo(map);
-  }
+        }, 500));
+
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            {
+                attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }
+        ).addTo(map);
+        var geoJsonLayer = await this.getFeaturesLayer();
+        geoJsonLayer.addTo(map);
+    },
+    methods: {
+        getFeaturesLayer: async function () {
+            // アイコン設定
+            var libraryIcon = L.icon({
+                iconUrl: './img/book.png',
+                iconSize: [40, 40],
+                iconAnchor: [25, 50],
+                popupAnchor: [0, -50],
+            });
+            var cafeIcon = L.icon({
+                iconUrl: './img/cafe.png',
+                iconSize: [40, 40],
+                iconAnchor: [25, 50],
+                popupAnchor: [0, -50],
+            });
+
+            const geojson = {
+                "type": "FeatureCollection",
+                "features": [],
+            }
+            var geoJsonLayer = L.geoJson(geojson,
+                {
+                    pointToLayer: function (feature, latlng) {
+                        // typeが図書館の場合は図書館のアイコンを表示する
+                        if(feature.properties.type == 'library'){
+                            return L.marker(latlng, {icon: libraryIcon})
+                        }
+                        // 図書館以外の場合はカフェのアイコンを表示する
+                        // TODO: 公民館が増えた場合は判定を追加する
+                        else{
+                            return L.marker(latlng, {icon: cafeIcon})
+                        }
+                    },
+                    onEachFeature: function(feature,layer){
+                        // Leafletのpopupからrouter-linkを表示することができない
+                        // そのため、Vueオブジェクトを作ってpopupに渡す
+                        let name = feature.properties.name;
+                        let PopupCont = Vue.extend({
+                            router,
+                            template: `<router-link to="/detail/${feature.id}" title="詳細">${name}</router-link>`
+                        })
+                        let popup = new PopupCont()
+                        layer.bindPopup(popup.$mount().$el)
+                    }
+                }
+            );
+            // geoJsonLayer.addTo(map);
+
+            var bboxes = ['0', 'z'];
+            if (false) {  // データ量が増えてきたら有効化する
+                // BoundingBoxで限定して検索する
+                const bounds = map.getBounds();
+                for (let precision = 1; precision < 8; precision++) {
+                    bboxes = geohash.bboxes(bounds.getSouth(), bounds.getWest(), bounds.getNorth(), bounds.getEast(), precision);
+                    if (bboxes.length > 8) break;
+                }
+                console.log(map.getZoom())
+                console.log(bboxes);
+            }
+
+            // retrieve features from firestore
+            const db = firebase.firestore();
+            const features = await db.collection('feature')
+                .where('geohash','>=', bboxes[0])
+                .where('geohash','<', bboxes[bboxes.length-1])
+                .get();
+            console.log("Feature Qty: ", features.docs.length)
+            features.docs.map(doc => {
+                const data = doc.data();
+                return data.geometry
+                    ? {  // from geopoint
+                        id: doc.id,
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [
+                                data.geometry.longitude,
+                                data.geometry.latitude,
+                            ],
+                        },
+                        "properties": {
+                            name: data.name,
+                            imgSrc: data.imgSrc,
+                            type: data.type,
+                        },
+                    }
+                    : Object.assign({id: doc.id}, JSON.parse(data.geojson || "{}"));  // from geojson
+            })
+                .map(data => geoJsonLayer.addData(data));
+
+            // retrieve features from spreadsheet
+            if (false) try {
+                await new Promise(resolve => setTimeout(resolve, 500));
+                while(!gapi.client.sheets) {
+                    console.debug("waiting for that gapi.client.sheets api be available...")
+                    await new Promise(resolve => setTimeout(resolve, 50));
+                }
+
+                const response = await gapi.client.sheets.spreadsheets.values.get({
+                    spreadsheetId: '1JWUkyuRUprYrODsa1Fr70VOIxleLNy5g3Xs-g7NymmI',
+                    range: 'amenity:library'
+                });
+                var features_json = response.result?.values?.slice(2)?.map(feature => {
+                    const [id, lat, lng, type, name, description, imgSrc] = feature;
+                    return {
+                        type: "Feature",
+                        id,
+                        properties: { "名称": name, type, name, description, imgSrc },
+                        geometry: {
+                            type: "Point",
+                            coordinates: [ lat, lng ]
+                        }
+                    };
+                }) || [];
+                geojson.features.push(...features_json);
+            } catch (error) {
+                // do nothing...
+            }
+            return geoJsonLayer;
+        },
+    }
 }
 </script>
 <style>
