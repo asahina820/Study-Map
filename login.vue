c422cd18926dbc468cdcd779f568855b3b122fb8
Akihiro MATOBA <matobaa+github@gmail.com>
2022-05-09 23:26:19 +0900

    機能追加: ログインユーザをソーシャルアカウントに紐づける
    https://firebase.google.com/docs/auth/web/account-linking?hl=ja

--- login.vue
+++ login.vue
@@ -15,7 +15,19 @@
                 <div class="item">emailVerified:
                     <div class="content">{{ user.emailVerified || "void" }} </div>
                 </div>
+                <div class="item">Social Login:<br />
+                    <div class="content" v-for="provider in providers.slice(1)" :key="provider.providerId">
+                        <i class="ui icon" :class="provider.providerId.slice(0, provider.providerId.length - 4)"></i>{{
+                                provider.providerId
+                        }}
+                        <span v-if="user.providerData.find(pd => pd.providerId === provider.providerId)">連携済み
+                            <button @click="user.unlink(provider.providerId).then(() => this.user = user)">解除</button>
+                        </span>
+                        <button v-else @click="firebase.auth().currentUser.linkWithRedirect(provider)">連携する</button>
+                    </div>
+                </div>
             </div>
+            <div>{{ error_message }}</div>
             <button @click="signout">sign out</button>
         </div>
     </div>
@@ -26,11 +38,32 @@ module.exports = {
     data: function () {
         return {
             user: undefined,
+            error_message: undefined,
         }
     },
+    created: function () {
+        this.firebase = firebase;
+    },
     mounted() {
+        this.providers = [
+            new firebase.auth.EmailAuthProvider(),
+            new firebase.auth.GoogleAuthProvider(),
+            new firebase.auth.TwitterAuthProvider(),
+            new firebase.auth.GithubAuthProvider(),
+        ];
         firebase.auth().onAuthStateChanged((user) => {
             this.user = user ? user : null;
+        });
+        firebase.auth().getRedirectResult().then((result) => {
+            if (result.credential) {
+                // debugger;
+                // Accounts successfully linked.
+                var credential = result.credential;
+                var user = result.user;
+                // ...
+            }
+        }).catch((error) => {
+            this.error_message = `ERROR: ${error.message}`;
         });
     },
     updated() {
@@ -39,16 +72,8 @@ module.exports = {
             // https://firebase.google.com/docs/auth/web/firebaseui?hl=ja#oauth_providers_google_facebook_twitter_and_github
             const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth())
             ui.start('#firebaseui-auth-container', {
-                signInOptions: [
-                    // List of OAuth providers supported.
-                    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-                    // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-                    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-                    firebase.auth.GithubAuthProvider.PROVIDER_ID
-                ],
+                signInOptions: this.providers.map(p => p.providerId),
                 signInSuccessUrl: window.location.origin + window.location.pathname,
-                // Other config options...
             });
         };
     },
@@ -56,7 +81,7 @@ module.exports = {
         async signout() {
             await firebase.auth().signOut();
             this.user = null;
-        }
+        },
     }
 }
 </script>
